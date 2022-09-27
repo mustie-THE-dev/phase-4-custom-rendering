325ff476459434f4fb296ed5e2c5824a9f64194b
Simon kamau <simon.kamau@student.moringaschool.com>
2022-09-27 17:09:06 +0300

    commit tree 03a8241752c1f48e1a0282897378725dd8300a62

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -12,4 +12,14 @@ class CheesesController < ApplicationController
     render json: cheese
   end
 
+  def show
+    cheese = Cheese.find_by(id: params[:id])
+    if cheese
+      render json: cheese, except: [:created_at, :updated_at], methods: [:summary]
+    else
+      # status: :not_found will produce a 404 status code
+      render json: { error: 'Cheese not found' }, status: :not_found
+    end
+  end
+
 end
